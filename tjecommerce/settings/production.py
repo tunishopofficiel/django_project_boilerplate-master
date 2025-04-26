6c16146d708f88bfb23b7432ffb97bdda4a01694
tunishopofficiel <Tunishop.official@gmail.com>
2025-04-26 22:45:08 +0100

    Update production.py

--- tjecommerce/settings/production.py
+++ tjecommerce/settings/production.py
@@ -10,14 +10,16 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
 ]
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        # this file will live next to manage.py
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
